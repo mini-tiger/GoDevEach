c259b7c869fc7f4e891fb94687df44ad64a81392
taojun <61566027@163.com>
2021-07-13 14:07:35 +0800

    Default Changelist

--- rabbitMQ/l1/conf/conf.go
+++ rabbitMQ/l1/conf/conf.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	RMQADDR     = "amqp://test:123456@127.0.0.1:5672/testVH"
+	RMQADDR     = "amqp://admin:admin@192.168.40.127:5672/"
 	QUEUENAME   = "testqueue"
 	PRODUCERCNT = 50
 	CONSUMERCNT = 5
