87da81b241a638225f69061763bda162b9787b4d
taojun <61566027@163.com>
2021-07-14 12:41:24 +0800

    Default Changelist

--- TcpUDP服务端客户端/zinxExample/client.go
+++ TcpUDP服务端客户端/zinxExample/client.go
@@ -8,6 +8,49 @@ import (
 	"time"
 )
 
+func sendData(conn net.Conn, msgid uint32) {
+
+	//发封包message消息
+	dp := znet.NewDataPack()
+	msg, _ := dp.Pack(znet.NewMsgPackage(msgid, []byte(fmt.Sprintf("Zinx Client Test Message msgid:%d", msgid)))) // msgid 要与server匹配
+	_, err := conn.Write(msg)
+	if err != nil {
+		fmt.Println("write error err ", err)
+		return
+	}
+
+	//先读出流中的head部分
+	headData := make([]byte, dp.GetHeadLen())
+	_, err = io.ReadFull(conn, headData) //ReadFull 会把msg填充满为止
+	if err != nil {
+		fmt.Println("read head error")
+
+	}
+	//将headData字节流 拆包到msg中
+	msgHead, err := dp.Unpack(headData)
+	if err != nil {
+		fmt.Println("server unpack err:", err)
+		return
+	}
+
+	if msgHead.GetDataLen() > 0 {
+		//msg 是有data数据的，需要再次读取data数据
+		msg := msgHead.(*znet.Message)
+		msg.Data = make([]byte, msg.GetDataLen())
+
+		//根据dataLen从io中读取字节流
+		_, err := io.ReadFull(conn, msg.Data)
+		if err != nil {
+			fmt.Println("server unpack data err:", err)
+			return
+		}
+
+		fmt.Println("==> Recv Msg: ID=", msg.Id, ", len=", msg.DataLen, ", data=", string(msg.Data))
+	}
+
+	time.Sleep(1 * time.Second)
+}
+
 /*
 	模拟客户端
 */
@@ -15,7 +58,7 @@ func main() {
 
 	fmt.Println("Client Test ... start")
 	//3秒之后发起测试请求，给服务端开启服务的机会
-	time.Sleep(3 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	conn, err := net.Dial("tcp", "127.0.0.1:7777")
 	if err != nil {
@@ -24,44 +67,9 @@ func main() {
 	}
 
 	for n := 10; n >= 0; n-- {
-		//发封包message消息
-		dp := znet.NewDataPack()
-		msg, _ := dp.Pack(znet.NewMsgPackage(0, []byte("Zinx Client Test Message")))
-		_, err := conn.Write(msg)
-		if err != nil {
-			fmt.Println("write error err ", err)
-			return
-		}
-
-		//先读出流中的head部分
-		headData := make([]byte, dp.GetHeadLen())
-		_, err = io.ReadFull(conn, headData) //ReadFull 会把msg填充满为止
-		if err != nil {
-			fmt.Println("read head error")
-			break
-		}
-		//将headData字节流 拆包到msg中
-		msgHead, err := dp.Unpack(headData)
-		if err != nil {
-			fmt.Println("server unpack err:", err)
-			return
-		}
-
-		if msgHead.GetDataLen() > 0 {
-			//msg 是有data数据的，需要再次读取data数据
-			msg := msgHead.(*znet.Message)
-			msg.Data = make([]byte, msg.GetDataLen())
-
-			//根据dataLen从io中读取字节流
-			_, err := io.ReadFull(conn, msg.Data)
-			if err != nil {
-				fmt.Println("server unpack data err:", err)
-				return
-			}
-
-			fmt.Println("==> Recv Msg: ID=", msg.Id, ", len=", msg.DataLen, ", data=", string(msg.Data))
-		}
-
-		time.Sleep(1 * time.Second)
+		//go sendData(conn,0)
+		go sendData(conn, 1)
+		time.Sleep(500 * time.Millisecond)
 	}
+	time.Sleep(10 * time.Second)
 }
--- TcpUDP服务端客户端/zinxExample/server.go
+++ TcpUDP服务端客户端/zinxExample/server.go
@@ -26,6 +26,22 @@ func (this *PingRouter) Handle(request ziface.IRequest) {
 	}
 }
 
+type PingRouter1 struct {
+	znet.BaseRouter
+}
+
+//Ping Handle
+func (this *PingRouter1) Handle(request ziface.IRequest) {
+	//先读取客户端的数据
+	fmt.Println("Recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
+
+	//xxx 再向client回写ping...ping...ping
+	err := request.GetConnection().SendBuffMsg(0, []byte("ping...ping...ping"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 	//1 创建一个server句柄
 
@@ -36,7 +52,7 @@ func main() {
 
 	//2 配置路由
 	s.AddRouter(0, &PingRouter{})
-
+	s.AddRouter(1, &PingRouter{})
 	//3 开启服务
 	s.Serve()
 }
