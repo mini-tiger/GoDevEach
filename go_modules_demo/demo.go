30243f9d03a31bbee9a0b928ee48b4f4c86a5c5d
taojun <61566027@163.com>
2021-07-23 17:29:10 +0800

    Default Changelist

--- go_modules_demo/demo.go
+++ go_modules_demo/demo.go
@@ -7,9 +7,9 @@ import (
 
 // xxx go mod 命令 https://colobu.com/2018/08/27/learn-go-module/
 
-//1. cd go_modules_demo && go mod init &&  go mod why # 自检
+//1. xxx cd go_modules_demo && go mod init &&  go mod why # 自检
 //2. 查找可用版本go list -m -versions github.com/gin-gonic/gin 或者 版本写 latest
-//3.  go mod download github.com/gin-gonic/gin  生成 go.sum 或者 go mod tidy
+//3. xxx go mod download github.com/gin-gonic/gin  生成 go.sum 或者 go mod tidy
 
 func main() {
 	// xxx 默认已经连接了 Logger and Recovery 中间件
