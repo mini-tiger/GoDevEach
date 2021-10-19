dedca4a26a6f74fd3b0e653c18e735e0c1792492
taojun <61566027@163.com>
2021-10-19 14:16:05 +0800

    Default Changelist

--- go_modules_demo/demo.go
+++ go_modules_demo/demo.go
@@ -8,7 +8,7 @@ import (
 // xxx go mod 命令 https://colobu.com/2018/08/27/learn-go-module/
 
 //1. xxx cd go_modules_demo && go mod init &&  go mod why # 自检
-//2. 查找可用版本go list -m -versions github.com/gin-gonic/gin 或者 版本写 latest
+//2. xxx 查找可用版本go list -m -versions github.com/gin-gonic/gin 或者 版本写 latest
 //3. xxx go mod download github.com/gin-gonic/gin  生成 go.sum 或者 go mod tidy
 
 func main() {
