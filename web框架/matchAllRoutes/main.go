141cb0130477ba1a6e6096195aecaa403deecbf3
tao.jun <61566027@163.com>
2022-02-14 16:53:07 +0800

    cronJob

--- web框架/matchAllRoutes/main.go
+++ web框架/matchAllRoutes/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	r := gin.Default()
-
+	r.ForwardedByClientIP = true
 	// 路由定义post请求, url路径为：/user/login, 绑定doLogin控制器函数
 	r.GET("/*path", doLogin)
 	r.Run()
@@ -21,8 +21,10 @@ func doLogin(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": gin.H{
-			"code":    http.StatusOK,
-			"message": fmt.Sprintf("method:%s,host:%v,path:%s", c.Request.Method, c.Request.Host, path),
+			"code":     http.StatusOK,
+			"message":  fmt.Sprintf("method:%s,host:%v,path:%s", c.Request.Method, c.Request.Host, path),
+			"clientIP": c.ClientIP(),
+			"header":   c.Request.Header,
 		},
 	})
 }
