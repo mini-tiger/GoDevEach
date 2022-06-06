559bfc3936b8e2283c224b29cbab16bcd2b2cf1b
tao.jun <tao.jun@neolink.com>
2022-06-06 09:23:24 +0800

    default

--- test_测试/linkedmap_test.go
+++ test_测试/linkedmap_test.go
@@ -6,23 +6,33 @@ import (
 	"testing"
 )
 
-func Benchmark_Linkmap(b *testing.B) {
-	a := lm.NewLinkedMap()
-	for i := 0; i < 2000; i++ {
-		a.Put(strconv.Itoa(i), i)
+var lmm = lm.NewLinkedMap()
+
+func loadData() {
+	for i := 0; i < 200; i++ {
+		lmm.Put(strconv.Itoa(i), i)
 	}
 
 	for i := 0; i < 100; i++ {
-		a.Remove(strconv.Itoa(i))
+		lmm.Remove(strconv.Itoa(i))
 	}
+}
+func Benchmark_Linkmap(b *testing.B) {
+	//a := lm.NewLinkedMap()
+
+	loadData()
+
 	//fmt.Println(a.Max())
 	//fmt.Printf("%+v\n", a.MData)
 	//fmt.Printf("%+v\n", a.MLink)
-	for _, key := range a.SortLinkMap() {
-		//fmt.Println(key)
-		a.Get(key)
-		//fmt.Printf("key:%s,value:%v\n", key, v)
+	//for _, key := range lmm.SortLinkMap() {
+	//fmt.Println(key)
+	//lmm.Get(key)
+	//fmt.Printf("key:%s,value:%v\n", key, v)
 
+	//}
+	for i := 0; i < b.N; i++ {
+		lmm.Get(strconv.Itoa(i))
 	}
 
 }
