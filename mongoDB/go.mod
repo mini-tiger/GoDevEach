b65f0e394b4e1442ffbcfcb49256eeb0fc945970
tao.jun <tao.jun@neolink.com>
2023-03-15 09:20:31 +0800

    commit

--- mongoDB/go.mod
+++ mongoDB/go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/beego/beego/v2 v2.0.4
+	github.com/google/uuid v1.2.0
 	go.mongodb.org/mongo-driver v1.10.6
 )
--- ttest/f.go
+++ ttest/f.go
@@ -1,38 +1,12 @@
 package main
 
-import "fmt"
-
-func GenSliceAppend() []int {
-	s := make([]int, 0, 100)
-	var ss []int
-	for i := 0; i < 100; i++ {
-		ss = append(ss, i)
-	}
-	// testpush
-	s = append(s, ss...)
-	return s
-}
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
 
 func main() {
-	s := GenSliceAppend()
-	fmt.Println(len(s), s[99])
-	append1()
-	copy1()
-}
-func append1() {
-	s := make([]int, 0, 100)
-	fmt.Printf(" GC took %v\n", s)
-	for j := 0; j < 10; j++ {
-		s = append(s, []int{j, j + 1, j + 2, j + 3, j + 4}...)
-
-	}
-	fmt.Printf(" GC took %v,%d\n", len(s), cap(s))
-}
-func copy1() {
-	s := make([]int, 3, 100)
-	ss := []int{1, 2, 3}
-
-	copy(ss, s)
-	fmt.Printf("%v\n", s)
-	fmt.Printf("%v\n", ss)
+	uuid := uuid.New()
+	key := uuid.String()
+	fmt.Println(key)
 }
